e3a8be9c5b3b902ace60b4e1bf98507fa68929f4
Amos Mwangi <50863748+AmosMwangi@users.noreply.github.com>
2020-01-13 09:53:05 +0300

    urls.py
    imported setings

--- award/urls.py
+++ award/urls.py
@@ -5,13 +5,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns=[
     path('',views.home, name = 'home'),
-    url(r'^proile/(?P<username>\w+)', views.profile, name='profile'),
-    url(r'^upload/$', vies.createproject, name='createproject'),
-    url(r'^search/', views.searchpost, name='searchpost'),
-    url(r'^updateprofile$', views.updateprofile, name='prfileupdate'),
-    url(r'^rate/(?P<project_id>\d+)',viws.rate_project, name='rate'),
-    url(r'^detail/(?P<project_id>\d+)',views.detail, name='detail'),
-    url(r'^api/project/$', views.ProjectList.as_view())
+    path('proile/(?P<username>\w+)', views.profile, name='profile'),
+    path('upload/', vies.createproject, name='createproject'),
+    path('search/', views.searchpost, name='searchpost'),
+    path('updateprofile$', views.updateprofile, name='prfileupdate'),
+    url('rate/(?P<project_id>\d+)',viws.rate_project, name='rate'),
+    url('detail/(?P<project_id>\d+)',views.detail, name='detail'),
+    path('api/project/$', views.ProjectList.as_view())
 ]
 
 
